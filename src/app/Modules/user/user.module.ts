47bedeb05b6a8e82af8249866e01a49d233ea9ec
harsh3608 <harsh3608@gmail.com>
2023-05-09 22:22:27 +0530

    initiated modules and added components in user module

---
+++ src/app/Modules/user/user.module.ts
@@ -0,0 +1,19 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { UserRoutingModule } from './user-routing.module';
+import { LoginComponent } from './login/login.component';
+import { RegistrationComponent } from './registration/registration.component';
+
+
+@NgModule({
+  declarations: [
+    LoginComponent,
+    RegistrationComponent
+  ],
+  imports: [
+    CommonModule,
+    UserRoutingModule
+  ]
+})
+export class UserModule { }
