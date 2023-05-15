8bd12bf86d2af1a0ea59166e4283e7c5c78c1d52
Harsh Patel <harsh3608@gmail.com>
2023-05-15 18:34:41 +0530

    added registration form

--- src/app/Modules/user/registration/registration.component.ts
+++ src/app/Modules/user/registration/registration.component.ts
@@ -1,10 +1,44 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
+import { Title } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-registration',
   templateUrl: './registration.component.html',
   styleUrls: ['./registration.component.css']
 })
-export class RegistrationComponent {
+export class RegistrationComponent implements OnInit{
+  registationForm!: FormGroup;
+  public showPassword: boolean = false;
 
+  constructor(
+    private title: Title,
+    private fb: FormBuilder
+  ) {}
+
+  ngOnInit(): void {
+    this.title.setTitle('Registration');
+    this.registationForm = this.fb.group({
+      email: new FormControl('', [Validators.required, Validators.email]),
+      password: new FormControl('', [Validators.required])
+    });
+
+  }
+
+  submitForm(){
+
+  }
+
+  public togglePasswordVisibility(): void {
+    this.showPassword = !this.showPassword;
+  }
+
+
+  //Getter functions to get for-values from form-controls
+  get Email(): FormControl {
+    return this.registationForm.get("email") as FormControl;
+  }
+  get Password(): FormControl {
+    return this.registationForm.get("password") as FormControl;
+  }
 }
